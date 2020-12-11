c9ae1e6cd2865b7543b9f37726029d74b2374a1d
Eric Jacobs <eric@jacobs.af>
2020-12-10 21:36:31 -0700

    day 3 part 1

---
+++ 3-toboggan-trajectory/part_1.rb
@@ -0,0 +1,106 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+###
+# --- Day 3: Toboggan Trajectory ---
+#
+# With the toboggan login problems resolved, you set off toward the airport.
+# While travel by toboggan might be easy, it's certainly not safe: there's very
+# minimal steering and the area is covered in trees. You'll need to see which
+# angles will take you near the fewest trees.
+#
+# Due to the local geology, trees in this area only grow on exact integer
+# coordinates in a grid. You make a map (your puzzle input) of the open squares
+# (.) and trees (#) you can see. For example:
+#
+# ..##.......
+# #...#...#..
+# .#....#..#.
+# ..#.#...#.#
+# .#...##..#.
+# ..#.##.....
+# .#.#.#....#
+# .#........#
+# #.##...#...
+# #...##....#
+# .#..#...#.#
+#
+# These aren't the only trees, though; due to something you read about once
+# involving arboreal genetics and biome stability, the same pattern repeats to
+# the right many times:
+#
+# ..##.........##.........##.........##.........##.........##.......  --->
+# #...#...#..#...#...#..#...#...#..#...#...#..#...#...#..#...#...#..
+# .#....#..#..#....#..#..#....#..#..#....#..#..#....#..#..#....#..#.
+# ..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#
+# .#...##..#..#...##..#..#...##..#..#...##..#..#...##..#..#...##..#.
+# ..#.##.......#.##.......#.##.......#.##.......#.##.......#.##.....  --->
+# .#.#.#....#.#.#.#....#.#.#.#....#.#.#.#....#.#.#.#....#.#.#.#....#
+# .#........#.#........#.#........#.#........#.#........#.#........#
+# #.##...#...#.##...#...#.##...#...#.##...#...#.##...#...#.##...#...
+# #...##....##...##....##...##....##...##....##...##....##...##....#
+# .#..#...#.#.#..#...#.#.#..#...#.#.#..#...#.#.#..#...#.#.#..#...#.#  --->
+#
+# You start on the open square (.) in the top-left corner and need to reach the
+# bottom (below the bottom-most row on your map).
+#
+# The toboggan can only follow a few specific slopes (you opted for a cheaper
+# model that prefers rational numbers); start by counting all the trees you
+# would encounter for the slope right 3, down 1:
+#
+# From your starting position at the top-left, check the position that is right
+# 3 and down 1. Then, check the position that is right 3 and down 1 from there,
+# and so on until you go past the bottom of the map.
+#
+# The locations you'd check in the above example are marked here with O where
+# there was an open square and X where there was a tree:
+#
+# ..##.........##.........##.........##.........##.........##.......  --->
+# #..O#...#..#...#...#..#...#...#..#...#...#..#...#...#..#...#...#..
+# .#....X..#..#....#..#..#....#..#..#....#..#..#....#..#..#....#..#.
+# ..#.#...#O#..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#..#.#...#.#
+# .#...##..#..X...##..#..#...##..#..#...##..#..#...##..#..#...##..#.
+# ..#.##.......#.X#.......#.##.......#.##.......#.##.......#.##.....  --->
+# .#.#.#....#.#.#.#.O..#.#.#.#....#.#.#.#....#.#.#.#....#.#.#.#....#
+# .#........#.#........X.#........#.#........#.#........#.#........#
+# #.##...#...#.##...#...#.X#...#...#.##...#...#.##...#...#.##...#...
+# #...##....##...##....##...#X....##...##....##...##....##...##....#
+# .#..#...#.#.#..#...#.#.#..#...X.#.#..#...#.#.#..#...#.#.#..#...#.#  --->
+#
+# In this example, traversing the map using this slope would cause you to
+# encounter 7 trees.
+#
+# Starting at the top-left corner of your map and following a slope of right 3
+# and down 1, how many trees would you encounter?
+
+###
+# Solution
+
+# Definitely gonna need nested arrays to make a coordinate plane. Rather than
+# dealing with duplication logic for the rows, I think I want to just write a
+# circular array and load the lines into that. Then, we can traverse right
+# infinitely.
+
+# So we start at (0,0), then traverse three right, and one down. Don't count
+# hits during the traversal, only at the beginning or end of each iteration
+
+# Don't try to slice or anything fancy with PoorlyMadeCircularArray#[]
+class PoorlyMadeCircularArray < Array
+  def [](index)
+    super(index % (self.count - 1))
+  end
+end
+
+rows = File.readlines('input', chomp: true).map do |row|
+  PoorlyMadeCircularArray.new.concat(row.chars)
+end
+
+x, y, hit_tree = 0, 0, 0
+loop do
+  x += 3
+  y += 1
+  break if y >= rows.count - 1
+  hit_tree += 1 if rows[y][x] == '#'
+end
+
+puts "hit trees: #{hit_tree}"
